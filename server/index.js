cb1cfb1c40c427c1c2a3d3b82d4d3364375bc871
utkarsh867 <utkarsh867@gmail.com>
2019-10-31 11:45:43 +0800

    web: serves using server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import server from "./server";
 const express = require("express");
-
+const path = require("path");
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "web", "build")));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "..", "web", "build", "index.html"));
+});
 
 server.applyMiddleware({ app });
 
