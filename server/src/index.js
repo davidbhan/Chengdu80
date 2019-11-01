31a723fecc7a8c4e5c4b1a4963b6fe2d30b81d8b
utkarsh867 <utkarsh867@gmail.com>
2019-11-01 13:12:29 +0800

    server: major change - compiles server using babel

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,15 @@
+import "@babel/polyfill";
 import server from "./server";
 const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "..", "web", "build")));
+app.use(express.static(path.join(__dirname, "..", "..", "web", "build")));
 
 server.applyMiddleware({ app });
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "..", "web", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "..", "web", "build", "index.html"));
 });
 
 app.listen(4000, () => {
